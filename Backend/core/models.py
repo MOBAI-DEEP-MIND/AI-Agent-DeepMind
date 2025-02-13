1cd51c7e6714de23770e099168c44d081ca1848c
ahmed <zaterahmed62@gmail.com>
2025-02-13 19:50:17 +0100

    dd

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-
 from .managers import CustomUserManager
 
 class Author(models.Model):
@@ -14,14 +11,13 @@ class Category(models.Model):
     name = models.CharField(max_length=255)    
 
 class Book(models.Model):
-    title = models.CharField(max_length=255,unique=True)
+    title = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    authors = models.ManyToManyField(Author,related_name="authors")
+    authors = models.ManyToManyField(Author, related_name="written_books")
     publisher = models.CharField(max_length=255)
     published_date = models.DateField()
-    categories = models.ManyToManyField(Category,related_name='books')
-    rating = models.FloatField(null=True,blank=True)
-
+    categories = models.ManyToManyField(Category, related_name='books')
+    rating = models.FloatField(null=True, blank=True)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
@@ -39,16 +35,15 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         return self.email
     
 class History(models.Model):
-    user = models.ForeignKey(CustomUser,related_name='user')
+    user = models.ForeignKey(CustomUser, related_name='history_entries', on_delete=models.CASCADE)
     query = models.TextField() 
-    books = models.ManyToManyField(Book,related_name='books')
-
+    books = models.ManyToManyField(Book, related_name='history_books')
 
 class Busket(models.Model):
-    book = models.ManyToManyField(Book,related_name='books')    
-    user = models.ForeignKey(CustomUser,related_name='user')
+    book = models.ManyToManyField(Book, related_name='busket_books')    
+    user = models.ForeignKey(CustomUser, related_name='busket_entries', on_delete=models.CASCADE)
 
 class Purchase(models.Model):
-    user = models.ForeignKey(CustomUser,related_name='user')   
-    book = models.ManyToManyField(Book)   
+    user = models.ForeignKey(CustomUser, related_name='purchases', on_delete=models.CASCADE)   
+    book = models.ManyToManyField(Book, related_name='purchase_books')   
     number_books = models.IntegerField()
