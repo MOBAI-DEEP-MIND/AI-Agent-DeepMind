7ea859a603ac3bb599909efdee1fea0fb6eeb235
wail <wailbnlh@gmail.com>
2025-02-14 00:47:31 +0100

    fix signup

--- Backend/user/urls.py
+++ Backend/user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import UserCreateView
+from .views import CreateUserView,EditUserView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 urlpatterns = [
-    path("signup/", UserCreateView.as_view(), name="signup"),
+    path("signup/", CreateUserView.as_view(), name="signup"),
+    path("edit-profile/", EditUserView.as_view(), name="edit"),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 
 ]
--- Backend/user/views.py
+++ Backend/user/views.py
@@ -1,22 +1,27 @@
-from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics
 from .serializers import UserSerializer
 from core.models import CustomUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-
-class UserCreateView(CreateAPIView):
-
-    serializer_class = UserSerializer
+class CreateUserView(APIView):
     queryset = CustomUser.objects.all()
-    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+
+class EditUserView(generics.UpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer 
     authentication_classes = [JWTAuthentication]
-
-
-    # def  create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     if serializer.is_valid(data=request.data):
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = [IsAuthenticated]
